8a107c2d6b7890b5618f589b0660f1861a39d3ef
radu-c-tag <radu.costinean@theaccessgroup.com>
2024-08-27 15:46:09 +0300

    feat: generate new objectId only if _id attribute exists

--- src/Relations/EmbedsMany.php
+++ src/Relations/EmbedsMany.php
@@ -230,8 +230,8 @@ class EmbedsMany extends EmbedsOneOrMany
     protected function associateNew($model)
     {
         // Create a new key if needed.
-        if (!$model->getKey()) {
-            $model->setAttribute($model->getKeyName(), new ObjectID());
+        if ($model->getKeyName() === '_id' && !$model->getAttribute('_id')) {
+            $model->setAttribute('_id', new ObjectID);
         }
 
         $records = $this->getEmbedded();
